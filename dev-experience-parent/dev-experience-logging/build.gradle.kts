23cdadf131f93c74b95eb5a4fa605541a72e83e6
Wagner Fernando Costa <wcosta.bsi@gmail.com>
2022-08-26 19:33:03 -0300

    initial project

--- dev-experience-parent/dev-experience-logging/build.gradle.kts
+++ dev-experience-parent/dev-experience-logging/build.gradle.kts
@@ -1,17 +1,89 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    `java-library`
+    `maven-publish`
     kotlin("jvm") version "1.7.10"
 }
 
 group = "com.de"
 version = "1.0-SNAPSHOT"
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "dev-experience-logging"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("My Library")
+                description.set("A concise description of my library")
+                url.set("http://www.example.com/library")
+                properties.set(mapOf(
+                    "myProp" to "value",
+                    "prop.with.dots" to "anotherValue"
+                ))
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("wfcosta")
+                        name.set("Wagner Fernando Costa")
+                        email.set("wcosta.bsi@gmail.com")
+                    }
+                    developer {
+                        id.set("vitormakino")
+                        name.set("Vitor Makino")
+                        email.set("xxx@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://example.com/my-library.git")
+                    developerConnection.set("scm:git:ssh://example.com/my-library.git")
+                    url.set("http://example.com/my-library/")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri(layout.buildDirectory.dir("repos/releases"))
+            val snapshotsRepoUrl = uri(layout.buildDirectory.dir("repos/snapshots"))
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+    }
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-log4j2
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
     testImplementation(kotlin("test"))
 }
 
